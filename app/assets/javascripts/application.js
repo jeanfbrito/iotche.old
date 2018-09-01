af6c8077692fedaa2ee4b447164b00d209fe76f0
Jean Brito <jeanfbrito@gmail.com>
2018-09-01 19:17:09 -0300

    add chartkick

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,4 +16,6 @@
 //= require jquery3
 //= require popper
 //= require bootstrap-sprockets
+//= require Chart.bundle
+//= require chartkick
 //= require_tree .
--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,4 +1,8 @@
 class DashboardController < ApplicationController
   def index
+    @readings = Device.first.readings.all.last(50)
+    @readings = @readings.map do |reading|
+      [reading.created_at, reading.data['temperature']]
+    end
   end
 end
